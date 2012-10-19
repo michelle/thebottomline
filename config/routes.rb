0f497e5b0663122cb09c1134ed3401326a62ee9e
ericz <really.ez@gmail.com>
2012-10-19 16:36:16 -0700

    add register controller + paths

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,9 @@ Thebottomline::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  match 'register' => 'register#new', :via => :get, :as => :register
+  match 'register' => 'register#create', :via => :post
+  
   root :to => 'welcome#index'
+  
 end
