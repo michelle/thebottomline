ad0188335a8ec68684e28ff176aa2aae502a0abc
Stephanie <saasbook@saasbook.(none)>
2012-10-19 19:00:18 -0700

    updated user_spec for forgot email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'bcrypt'
 
 describe User do
   user = User.create!(:name=>"President Skroob",
@@ -13,15 +14,27 @@ describe User do
   describe 'Validating a password' do
     describe 'Incorrect password' do
       it 'should return nil' do
+        User.stub(:find_by_email).and_return(user)
         valid_user = User.valid_user("skroobydoo@spaceballs.com", "1234")
         valid_user.should == nil
       end
     end
     describe 'Correct password' do
       it 'should return the user' do
+        User.stub(:find_by_email).and_return(user)
         valid_user = User.valid_user("skroobydoo@spaceballs.com", "12345")
         valid_user.should == user
       end
+    end
+  end
+  describe 'User forgot email' do
+    it 'should' do
+      User.stub(:find_by_email).and_return(user)
+      UserMailer.stub(:create_and_deliver_password_change)
+      old_user_password = user.password
+      User.forgot_password(user.email)
+      user_in_database = User.find(user.id)
+      user_in_database.password.should_not == old_user_password
     end
   end
 end
