ae0afdaf0eb480b3e3de2aa27d62be87d66adaea
ystephie <ystephie@berkeley.edu>
2012-10-19 18:20:31 -0700

    added forgot password in model and mailer for forgot password

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,12 @@ class User < ActiveRecord::Base
     my_password = BCrypt::Password.new(user.password)
     return my_password == password
   end
+  
+  def forgot_password(email)
+    user = User.find_by_email(email)
+    random_password = Array.new(10).map { (65 + rand(58)).chr }.join
+    user.password = random_password
+    user.save!
+    UserMailer.create_and_deliver_password_change(user,random_password)
+  end
 end
