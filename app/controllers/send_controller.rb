6f814e0143dd2e54dd9f2510a54a59f59b511b41
ericz <really.ez@gmail.com>
2012-11-02 21:33:50 -0700

    1stv send_controller

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -1,3 +1,68 @@
+class SendController < ApplicationController
+
+  def index
+    render "index"
+  end
+  
+  def ecard_new
+    @recent = @user.get_recent_postcards(5);
+    render "ecard"
+  end
+  
+  def ecard_create
+    @ecard = Ecard.new params[:card]
+    @valid = @ecard.valid?
+    if not @valid
+      flash[:error] = @ecard.errors.full_messages.concat(errors).join "<br>"
+      redirect_to send_ecard_path
+    else
+      flash[:notice] = "Yay! Your E-card has been sent"
+      @ecard.save
+      redirect_to send_path
+    end
+  end
+  
+  def postcard_new
+    @user = User.find_by_id(session[:userid])
+    if @user.nil?
+      flash[:error] = 'You must be logged in to send postcards. <a href="'+ register_path +'">Register here</a>'
+      redirect_to login_path
+      return
+    elsif not @user.can_send_postcard
+      flash[:error] = 'You\'ve already reached the limit of two postcards per year. <a href="' + send_ecard_path + '">Send an ecard instead</a>'
+      redirect_to send_path
+      return
+    end
+    @recent = @user.get_recent_postcards(5);
+    render "postcard"
+  end
+  
+  def postcard_create
+    @user = User.find_by_id(session[:userid])
+    if @user.nil?
+      flash[:error] = 'You must be logged in to send postcards. <a href="'+ register_path +'">Register here</a>'
+      redirect_to login_path
+      return
+    elsif not @user.can_send_postcard
+      flash[:error] = 'You\'ve already reached the limit of two postcards per year. <a href="' + send_ecard_path + '">Send an ecard instead</a>'
+      redirect_to send_path
+      return
+    end
+    @postcard = Postcard.new params[:card]
+    @valid = @postcard.valid?
+    if not @valid
+      flash[:error] = @postcard.errors.full_messages.concat(errors).join "<br>"
+      redirect_to send_postcard_path
+    else
+      flash[:notice] = "Yay! Your postcard has been sent"
+      @postcard.save
+      redirect_to send_path
+    end
+  end
+  
+end
+
+
 # POST:
 # check:
 # logged in?
@@ -13,16 +78,7 @@
 #	send the card!
 #
 #
-# GET:
-# check:
-# logged in?
-# for postcards: User.can_send_postcard; can't send postcards at all if not logged in.
-# if they can't send postcard, redirect to /send with flash message
-#
-# /send = index ... reminder page that has two buttons
-#
-# /send/ecard
-# /send/postcard
+# 
 #
 # User.get_recent_ecards(num of recent ecards)
 #  '' '''' postcards( '''' )
