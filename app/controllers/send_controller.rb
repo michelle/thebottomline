2b5d72f1d75a78bf4109a034b78177c678d436e7
Michelle Bu <analogmidnight@gmail.com>
2012-11-03 00:49:02 -0700

    finished both postcard and ecard views, added TODOs to controller and commented out a few things so that it doesnt crash. waiting on appropriate model functions

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -1,7 +1,8 @@
 class SendController < ApplicationController
 
-  RECENT_COUNT = 5
+	# TODO(ericz): Save the DATE of the ecard/postcard sending.
 
+  RECENT_COUNT = 5
   def index
     render "index"
   end
@@ -9,7 +10,7 @@ class SendController < ApplicationController
   def ecard_new
     @user = User.find_by_id(session[:userid])
     if not @user.nil?
-      @recent = @user.get_recent_ecards(SendController::RECENT_COUNT);
+      @recent = [] #@user.get_recent_ecards(SendController::RECENT_COUNT);
     end
     render "ecard"
   end
@@ -23,6 +24,7 @@ class SendController < ApplicationController
     else
       flash[:notice] = "Yay! Your E-card has been sent"
       @ecard.save
+      # TODO(ericz): Actually send the e-card.
       redirect_to send_path
     end
   end
@@ -33,12 +35,12 @@ class SendController < ApplicationController
       flash[:error] = "You must be logged in to send postcards. <a href=\"#{register_path}\">Register here</a>"
       redirect_to login_path
       return
-    elsif not @user.can_send_postcard
+    elsif not true #@user.can_send_postcard
       flash[:error] = "You've already reached the limit of two postcards per year. <a href=\"#{send_ecard_path}\">Send an ecard instead</a>"
       redirect_to send_path
       return
     end
-    @recent = @user.get_recent_postcards(SendController::RECENT_COUNT);
+    @recent = [] #@user.get_recent_postcards(SendController::RECENT_COUNT);
     render "postcard"
   end
   
@@ -54,6 +56,7 @@ class SendController < ApplicationController
       return
     end
     @postcard = Postcard.new params[:card]
+    @postcard.address = params[:address_street] + ',' + params[:address_city] + ',' + params[:address_state] + ',' + params[:address_zip]
     @valid = @postcard.valid?
     if not @valid
       flash[:error] = @postcard.errors.full_messages.join "<br>"
@@ -61,10 +64,69 @@ class SendController < ApplicationController
     else
       flash[:notice] = "Yay! Your postcard has been sent"
       @postcard.save
+      # TODO(ericz): Actually save the file as a CSV of sorts.
       redirect_to send_path
     end
   end
   
+  helper_method :us_states
+  
+  def us_states
+    [
+      ['AK', 'AK'],
+      ['AL', 'AL'],
+      ['AR', 'AR'],
+      ['AZ', 'AZ'],
+      ['CA', 'CA'],
+      ['CO', 'CO'],
+      ['CT', 'CT'],
+      ['DC', 'DC'],
+      ['DE', 'DE'],
+      ['FL', 'FL'],
+      ['GA', 'GA'],
+      ['HI', 'HI'],
+      ['IA', 'IA'],
+      ['ID', 'ID'],
+      ['IL', 'IL'],
+      ['IN', 'IN'],
+      ['KS', 'KS'],
+      ['KY', 'KY'],
+      ['LA', 'LA'],
+      ['MA', 'MA'],
+      ['MD', 'MD'],
+      ['ME', 'ME'],
+      ['MI', 'MI'],
+      ['MN', 'MN'],
+      ['MO', 'MO'],
+      ['MS', 'MS'],
+      ['MT', 'MT'],
+      ['NC', 'NC'],
+      ['ND', 'ND'],
+      ['NE', 'NE'],
+      ['NH', 'NH'],
+      ['NJ', 'NJ'],
+      ['NM', 'NM'],
+      ['NV', 'NV'],
+      ['NY', 'NY'],
+      ['OH', 'OH'],
+      ['OK', 'OK'],
+      ['OR', 'OR'],
+      ['PA', 'PA'],
+      ['RI', 'RI'],
+      ['SC', 'SC'],
+      ['SD', 'SD'],
+      ['TN', 'TN'],
+      ['TX', 'TX'],
+      ['UT', 'UT'],
+      ['VA', 'VA'],
+      ['VT', 'VT'],
+      ['WA', 'WA'],
+      ['WI', 'WI'],
+      ['WV', 'WV'],
+      ['WY', 'WY']
+    ]
+  end
+
 end
 
 
