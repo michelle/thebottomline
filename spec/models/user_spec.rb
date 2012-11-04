aaaec9a5ca53918d53495a3de3d01352147a33cc
Stephanie <saasbook@saasbook.(none)>
2012-11-04 11:18:24 -0800

    added rspec tests for new user card methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,9 +38,38 @@ describe User do
     end
   end
   describe 'Get recent ecards' do
+    it 'should get the most recently created AMOUNT ecards' do
+      user.get_recent_ecards(1).count.should == 0
+      user.ecards.create(:recipient => "grandma", :address => "koolgrandma103@gmail.com", :sender => "grandson")
+      ecards = user.get_recent_ecards(1)
+      ecards.count.should == 1
+      ecards[0].recipient.should be "grandma"
+      user.ecards.create(:recipient => "grandpa", :address => "koolgrandpa103@gmail.com", :sender => "grandson")
+      ecards = user.get_recent_ecards(1)
+      ecards.count.should == 1
+      ecards[0].recipient.should be "grandpa"
+    end
   end
   describe 'Get recent postcards' do
+    it 'should get the most recently created AMOUNT postcards' do
+      user.get_recent_postcards(1).count.should == 0
+      user.postcards.create(:recipient => "grandma", :address => "2316 Haste St, Berkeley, CA 94704", :sender => "grandson")
+      postcards = user.get_recent_postcards(1)
+      postcards.count.should == 1
+      postcards[0].recipient.should be "grandma"
+      user.postcards.create(:recipient => "grandpa", :address => "2316 Haste St, Berkeley, CA 94704", :sender => "grandson")
+      postcards = user.get_recent_postcards(1)
+      postcards.count.should == 1
+      postcards[0].recipient.should be "grandpa"
+    end
   end
   describe 'Check if user can send more postcards' do
+    it 'should return true if user has less than 2 postcards, false o/w' do
+      user.can_send_postcard?.should be true
+      user.postcards.create(:recipient => "grandma", :address => "2316 Haste St, Berkeley, CA 94704", :sender => "grandson")
+      user.can_send_postcard?.should be true
+      user.postcards.create(:recipient => "grandpa", :address => "2316 Haste St, Berkeley, CA 94704", :sender => "grandson")
+      user.can_send_postcard?.should be false
+    end
   end
 end
